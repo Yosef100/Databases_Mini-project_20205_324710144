8780cd0b7646c28812e7a3fa37a5b48cf1a9929e
Yosef100 <60045365+Yosef100@users.noreply.github.com>
2025-09-08 11:48:07 +0000

    deleted multi query files

--- q5.sql
+++
@@ -1,9 +0,0 @@
-\o C:/sql/q5results.txt
-
-UPDATE employee
-SET active = FALSE
-WHERE termination_date IS NOT NULL
-  AND termination_date <= CURRENT_DATE
-  AND active = TRUE;
-
-\o
--- q8.sql
+++
@@ -1,7 +0,0 @@
-\o C:/sql/q8results.txt
-
-DELETE FROM department d
-WHERE NOT EXISTS (SELECT 1 FROM position p WHERE p.department_id = d.department_id)
-  AND NOT EXISTS (SELECT 1 FROM employee  e WHERE e.department_id = d.department_id);
-
-\o
